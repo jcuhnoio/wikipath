4051866b75bcbf2932a0bc43057ad3f2161a5a47
Dexter Friis-Hecht <dfriishecht@olin.edu>
2024-10-24 10:21:06 -0400

    Add template for AStar graph

---
+++ a_star.py
@@ -0,0 +1,18 @@
+from graph import Graph
+from heapq import heapify, heappop, heappush
+
+class AStar(Graph):
+    def __init__(self, graph_dict: dict) -> None:
+        super().__init__(graph_dict)
+
+        self.graph = graph_dict
+        self.g = 0
+        self.f = 0
+        self.h = 0
+    
+    def find_path():
+        return
+    
+    def find_dist():
+        return
+    

625769d72d94aa84ce122fb6b831b18973782afb
Dokyun <donny5557@gmail.com>
2024-10-27 22:35:38 -0400

    remove old test graph

--- dijkstra.py
+++ dijkstra.py
@@ -129,16 +129,6 @@ class Dijkstra(Graph):
 
 
 if __name__ == "__main__":
-    test_graph = {
-        "A": {"B": 3, "C": 3},
-        "B": {"A": 3, "D": 3.5, "E": 2.8},
-        "C": {"A": 3, "E": 2.8, "F": 3.5},
-        "D": {"B": 3.5, "E": 3.1, "G": 10},
-        "E": {"B": 2.8, "C": 2.8, "D": 3.1, "G": 7},
-        "F": {"G": 2.5, "C": 3.5},
-        "G": {"F": 2.5, "E": 7, "D": 10},
-    }
-
     dijk = Dijkstra({})
     result = dijk.find_shortest_path(start="Alnico", goal="Magnetic field")
     print(result)
